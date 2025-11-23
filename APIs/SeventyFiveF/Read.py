ed05fa587a2dd13ba65f27c1079622773ceffa01
Peter Knall <pknall@aesbuildingservices.com>
2025-11-23 12:58:23 -0500

    Updated to be provided an the Authentication Key.

--- APIs/SeventyFiveF/Read.py
+++ APIs/SeventyFiveF/Read.py
@@ -1,15 +1,33 @@
 import json
 import requests
+import logging
+
+logger = logging.getLogger(__name__)
 
 class Read:
+    """
+    Do not use directly, use either ReadByFilter, ReadByFilterPaged, or ReadById.
+    """
     def __init__(self, username, password, subscription_key, authentication_key):
+        """
+        Class parent constructor.
+        :param username: Facilisight Username (required API privileges)
+        :param password: Facilisight Password
+        :param subscription_key: Subscription key from the API website
+        :param authentication_key: Authentication key provided by Facilisight through the Auth class.
+        """
         self.username = username
         self.password = password
         self.subscription_key = subscription_key
         self.authentication_key = authentication_key
         self.url = "https://api.75f.io/haystack/read"
 
-    def post(self):
+    def read(self):
+        """
+        Sends information to 75F API using an HTTPS Post call and returns the result as JSON object.
+        :return: JSON object with data from 75F API
+        :exception: Returns an empty string
+        """
         self.hdr = {
             'Authorization': self.authentication_key,
             'Accept': 'application/json',
@@ -17,12 +35,18 @@ class Read:
             'Cache-Control': 'no-cache',
             'Ocp-Apim-Subscription-Key': self.subscription_key,
         }
-        data = self.get_body()
+        data = self.get_body() # Call to function in child class
         try:
             response = requests.post(self.url, data=data, headers=self.hdr, timeout=30)
             return json.loads(response.text)
         except Exception as e:
-            raise Exception(f"Exception during SeventyFiveF.Read(): {e}")
+            logger.error(f"Exception during SeventyFiveF.Read(): {e}")
+            return ""
 
     def get_body(self):
-        raise Exception("Read should not be used directly.  Use one of the 'ReadyBy' children instead.")
+        """
+        Something bad has happened, you should not be here.
+        :return: Empty string
+        """
+        logger.error("Read should not be used directly.  Use one of the 'ReadyBy' children instead.")
+        return ""
