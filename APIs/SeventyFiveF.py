3b443f72e83675f41fd3d215fe57fc63d1db86f0
Peter Knall <pknall@aesbuildingservices.com>
2025-11-23 12:00:12 -0500

    Initial commit

--- APIs/SeventyFiveF.py
+++
@@ -1,7 +0,0 @@
-
-
-class API():
-    def __init__(self, username, password, subscription_key):
-        self.username = username
-        self.password = password
-        self.subscription_key = subscription_key
