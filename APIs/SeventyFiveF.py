74081e320c224177fadcf7697bf8a9ab6fd4187f
Peter Knall <pknall@aesbuildingservices.com>
2025-11-23 11:45:26 -0500

    Added access credentials to constructor

--- APIs/SeventyFiveF.py
+++ APIs/SeventyFiveF.py
@@ -1,5 +1,7 @@
 
 
 class API():
-    def __init__(self):
-        pass
+    def __init__(self, username, password, subscription_key):
+        self.username = username
+        self.password = password
+        self.subscription_key = subscription_key
