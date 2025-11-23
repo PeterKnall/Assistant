501cb4adcd6240314c4bfc1af2da98027e8be356
Peter Knall <pknall@aesbuildingservices.com>
2025-11-23 13:50:48 -0500

    Added Site ID read and TODO notes.

--- Main.py
+++ Main.py
@@ -1,5 +1,8 @@
 import os
+import pandas as pd
+import json
 import APIs.SeventyFiveF.Auth as SeventyFiveFAuth
+import APIs. SeventyFiveF.ReadByFilter as SeventyFiveFReadByFilter
 import logging
 
 logging.basicConfig(
@@ -12,18 +15,30 @@ class MPL:
     def init(self):
         pass
 
-    def startup(self):
+    def get_credentials_and_authorization_key(self):
         logging.debug("Entering startup()")
         # These values are stored in the Windows Environmental Variables so they can be accessed during runtime
         # rather than having them publicly viewable on GitHub.
         self.username = os.environ.get("75F API Username")                                   # 75F Facilisite username
         self.password = os.environ.get("75F API Password")                                   # 75F Facilisite password
-        self.subscription_key = os.environ.get("75F API Subscription Key")                    # API Key from 75F API Management portal
+        self.subscription_key = os.environ.get("75F API Subscription Key")                   # API Key from 75F API Management portal
 
         auth = SeventyFiveFAuth.Auth()
         self.authorization_key = auth.get_authorization_key(self.username, self.password, self.subscription_key)
 
+    def get_site_ids(self):
+        query_text = "building and equip"
+        reader = SeventyFiveFReadByFilter.ReadByFilter(self.username, self.password, self.subscription_key,
+                                                       self.authorization_key, query_text)
+
+        # TODO: Should be "reader.read("building and equip")
+        return reader.read()
+
 if __name__ == "__main__":
     logging.info("*** Application starting ***")
     mpl = MPL()
-    mpl.startup()
+    mpl.get_credentials_and_authorization_key()
+    sites_json = mpl.get_site_ids()
+    print(json.dumps(sites_json, indent=4, sort_keys=False))
+    sites_df = pd.DataFrame(sites_json["rows"])
+    sites_df.to_csv("site_df.csv", header=True, index=False)
