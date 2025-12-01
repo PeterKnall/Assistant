8ac2d14578a2f008a33492b668bb9677590495c6
Peter Knall <peterknall@gmail.com>
2025-12-01 10:33:48 -0500

    Added test for latest, yesterday, and today

--- Test/SeventyFiveF/test_hisReadMany.py
+++ Test/SeventyFiveF/test_hisReadMany.py
@@ -11,7 +11,8 @@ class Test_hisReadMany_date(TestCase):
         1) Privileges of username in Facilisite and the 75F API
         2) The date chosen still has trends (may need to be updated if values for these historical value have been archived)
         3) The id being used (while change if the current profile is changed or deleted)
-        4) The date cannot be the current date as there haven't been 1440 trends recorded yet
+        4) The date cannot be the current date as there haven't been 1440 trends recorded yet (current date is covered
+           with the "today" date range)
         """
         username = os.environ.get("75F API Username")
         password = os.environ.get("75F API Password")
@@ -33,7 +34,7 @@ class Test_hisReadMany_date(TestCase):
         1) Privileges of username in Facilisite and the 75F API
         2) The date chosen still has trends (may need to be updated if values for these historical value have been archived)
         3) The id being used (while change if the current profile is changed or deleted)
-        4) The date cannot include the current date as there haven't been 588 trends recorded yet
+        4) The date cannot include the current date as there haven't been 588 trends recorded yet (spanning two days)
 
         Notes: this feature returns historical values at 5 minute intervals instead of 1 minute intervals.
         """
@@ -57,7 +58,7 @@ class Test_hisReadMany_date(TestCase):
         1) Privileges of username in Facilisite and the 75F API
         2) The date chosen still has trends (may need to be updated if values for these historical value have been archived)
         3) The id being used (while change if the current profile is changed or deleted)
-        4) The date cannot include the current date as there haven't been 588 trends recorded yet
+        4) The date cannot include the current date as there haven't been 588 trends recorded yet (spanning two days)
 
         Notes: this feature returns historical values at 5 minute intervals instead of 1 minute intervals.
         """
@@ -73,4 +74,66 @@ class Test_hisReadMany_date(TestCase):
         df = pd.DataFrame(results['rows'][0])
 
         self.assertEqual(588, df.shape[0])
+
+    def test_hisReadMany_Latest(self):
+        """
+        This test checks that there is 1 trend returned when looking for the latest value.  This test depends on:
+        1) Privileges of username in Facilisite and the 75F API
+        2) The date chosen still has trends (may need to be updated if values for these historical value have been archived)
+        3) The id being used (while change if the current profile is changed or deleted)
+        """
+        username = os.environ.get("75F API Username")
+        password = os.environ.get("75F API Password")
+        subscription_key = os.environ.get("75F API Subscription Key")
+        ids = ["52bdc021-71d3-4479-903e-0b0986a993ee"]
+        date_range = "latest"
+
+        authentication_key = Auth.Auth().get_authorization_key(username, password, subscription_key)
+        reader = rm.hisReadMany(username, password, subscription_key, authentication_key)
+        results = reader.read(ids, date_range)
+        df = pd.DataFrame(results['rows'][0])
+
+        self.assertEqual(1, df.shape[0])
+
+    def test_hisReadMany_Today(self):
+        """
+        This test checks that some trends are returned when looking for today's values.  This test depends on:
+        1) Privileges of username in Facilisite and the 75F API
+        2) The date chosen still has trends (may need to be updated if values for these historical value have been archived)
+        3) The id being used (while change if the current profile is changed or deleted)
+        4) The date cannot include the current date as there haven't been 588 trends recorded yet
+        5) The test is not run at midnight (some trends will need to be recorded)
+        """
+        username = os.environ.get("75F API Username")
+        password = os.environ.get("75F API Password")
+        subscription_key = os.environ.get("75F API Subscription Key")
+        ids = ["52bdc021-71d3-4479-903e-0b0986a993ee"]
+        date_range = "today"
+
+        authentication_key = Auth.Auth().get_authorization_key(username, password, subscription_key)
+        reader = rm.hisReadMany(username, password, subscription_key, authentication_key)
+        results = reader.read(ids, date_range)
+        df = pd.DataFrame(results['rows'][0])
+
+        self.assertLessEqual(1, df.shape[0])
+
+    def test_hisReadMany_Yesterday(self):
+        """
+        This test checks that there are 1,440 trends returned for yesterday.  This test depends on:
+        1) Privileges of username in Facilisite and the 75F API
+        2) The date chosen still has trends (may need to be updated if values for these historical value have been archived)
+        3) The id being used (while change if the current profile is changed or deleted)
+        """
+        username = os.environ.get("75F API Username")
+        password = os.environ.get("75F API Password")
+        subscription_key = os.environ.get("75F API Subscription Key")
+        ids = ["52bdc021-71d3-4479-903e-0b0986a993ee"]
+        date_range = "yesterday"
+
+        authentication_key = Auth.Auth().get_authorization_key(username, password, subscription_key)
+        reader = rm.hisReadMany(username, password, subscription_key, authentication_key)
+        results = reader.read(ids, date_range)
+        df = pd.DataFrame(results['rows'][0])
+
+        self.assertEqual(1440, df.shape[0])
 
