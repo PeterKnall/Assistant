11a0edc8b1d07a6e28f18f7115453ecfa1333899
Peter Knall <peterknall@gmail.com>
2025-12-02 09:22:54 -0500

    Added call test for plot_single_axis()

--- Test/Tools/test_Charts.py
+++ Test/Tools/test_Charts.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 import Tools.Charts as tools
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 class TestCharts(TestCase):
@@ -11,3 +13,15 @@ class TestCharts(TestCase):
             self.fail(f"Instantiation failed: {e}")
 
         self.assertIsInstance(obj, tools.Charts)
+
+    def test_charts_call_plot_single_axis(self):
+        try:
+            obj = tools.Charts()
+            obj.plot_single_axis(None, None)
+        except Exception as e:
+            self.fail(f"Failed to call plot_single_axis(): {e}")
+
+
+
+
+
--- Tools/Charts.py
+++ Tools/Charts.py
@@ -1,8 +1,27 @@
-
+import numpy as np
+import matplotlib.pyplot as plt
 
 class Charts:
     """
-    Charting tools to handle data returned from various Building Automation System APIs
+    Charting tools to handle data returned from various Building Automation System APIs.  Notes on creating charts:
+    1) There are one or more child charts (axes) that are a part of a parent chart (figure).
+    2) Each chart requires a title.
+    3) Each chart may have different units for x and y values
     """
     def __init__(self):
+        pass
+
+    def plot_single_axis(self, ax, my_dict):
+        """
+        Plots a single axis of a figure using the values in the dictionary "dict".  There can be multiple plots
+        on a single axis.
+
+        :param ax: Reference to axis to plot
+        :param dict: Dictionary containing information to plot.  Key:Value pairs are:
+            name:   string
+            title:
+            kind:   string that is in ["line", "bar", "scatter", "line"]
+            data:   List of dictionary x, y name:value pairs
+        :return: updated axis
+        """
         pass
