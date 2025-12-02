54e053fe00a2bff0941228ac13cdbd910ffad64e
Peter Knall <peterknall@gmail.com>
2025-12-02 08:19:34 -0500

    Added instantiation test

--- Test/Tools/test_Charts.py
+++ Test/Tools/test_Charts.py
@@ -1,5 +1,15 @@
 from unittest import TestCase
 
+import Tools.Charts
+import Tools.Charts as tools
+
 
 class TestCharts(TestCase):
-    pass
+
+    def test_Charts_can_instantiate(self):
+        try:
+            obj = tools.Charts()
+        except Exception as e:
+            self.fail(f"Instantiation failed: {e}")
+
+        self.assertIsInstance(obj, tools.Charts)
